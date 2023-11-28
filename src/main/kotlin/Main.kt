2e3a1e1fe1fcf6c855e9c9d23700e4e605573f77
James Menzies <james.r.menzies@gmail.com>
2023-11-28 21:00:44 +1100

    add info command to CLI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,51 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.google.gson.Gson;
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.nio.file.Files
 import java.nio.file.Paths
 
 
-val mapper = ObjectMapper()
+val mapper = ObjectMapper().registerKotlinModule()
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class TorrentMetadata(
+    val announce: String,
+    val info: TorrentMetadataInfo
+
+){
+   data class TorrentMetadataInfo(
+       val length: Long,
+       val name: String,
+
+       @get:JsonProperty("piece length")
+       val pieceLength: String,
+       val pieces: String
+   )
+}
 
 fun main(args: Array<String>) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     val command = args[0]
     when (command) {
         "decode" -> {
-             val bencodedValue = args[1]
-             val decoded = decodeBencode(bencodedValue)
-             println(gson.toJson(decoded.first))
-             return
+            val bencodedValue = args[1]
+            val decoded = decodeBencode(bencodedValue)
+            println(mapper.writeValueAsString(decoded))
+            return
         }
+
         "info" -> {
             val filepath = Paths.get(args[1])
             val data = Files.readAllBytes(filepath)
-            val bencodedValue = data.map { Char(it.toInt() and 0xFF ) }.joinToString("")
-            val decoded = decodeBencode(bencodedValue)
-            val json = gson.toJsonTree(decoded)
+            val bencodedValue = data.map { Char(it.toInt() and 0xFF) }.joinToString("")
 
-            json.asJsonObject.get("announce").let { println("Tracker URL: $it") }
-
-            json.asJsonObject.get("info").let {
-                json.asJsonObject.get("length").let { println("Length: $it")}
-            }
+            val decoded = decodeBencode(bencodedValue).first
+            val metadata = mapper.convertValue(decoded, TorrentMetadata::class.java)
+            println("Tracker URL: ${metadata.announce}")
+            println("Length: ${metadata.info.length}")
         }
+
         else -> println("Unknown command $command")
     }
 }
@@ -43,14 +60,16 @@ fun decodeBencode(bencodedString: String): DecodeResult {
             val terminal = firstColonIndex + 1 + length
             val stringResult = bencodedString.substring(firstColonIndex + 1, terminal)
             val remainder = bencodedString.substring(terminal)
-            Pair(stringResult, remainder )
+            Pair(stringResult, remainder)
         }
+
         bencodedString[0] == 'i' -> {
             val terminal = bencodedString.indexOfFirst { it == 'e' }
             val integerResult = bencodedString.substring(1, terminal).toLong()
             val remainder = bencodedString.substring(terminal + 1)
             Pair(integerResult, remainder)
         }
+
         bencodedString[0] == 'l' -> {
             val result = mutableListOf<Any?>()
             var remainder: String = bencodedString.substring(1)
@@ -64,6 +83,7 @@ fun decodeBencode(bencodedString: String): DecodeResult {
             }
             Pair(result, remainder.substring(1))
         }
+
         bencodedString[0] == 'd' -> {
             val result = mutableMapOf<String, Any?>()
             var remainder: String = bencodedString.substring(1)
@@ -78,6 +98,7 @@ fun decodeBencode(bencodedString: String): DecodeResult {
 
             Pair(result, remainder.substring(1))
         }
+
         else -> throw Exception("Unexpected character when parsing payload: $bencodedString[0]")
     }
 }
